7e3f838169c66a1169ad1e9a95ccd9a560238956
Aditya Bisht <adityabisht894@gmail.com>
2025-06-05 15:07:19 +0530

    once-more

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ const isDevelopment = import.meta.env.MODE === 'development';
 
 export const API_BASE_URL = isDevelopment 
   ? 'http://localhost:5002/api'
-  : 'https://postman-backend-six.vercel.app/';
+  : 'https://postman-backend-six.vercel.app/api';
 
 export const ENDPOINTS = {
   SPEAKERS: `${API_BASE_URL}/speakers`,
